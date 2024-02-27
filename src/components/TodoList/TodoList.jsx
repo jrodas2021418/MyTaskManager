1b08d01716bf3c7ddb947033c80e1296f7a53b6c
jrodas2021418 <107076089+jrodas2021418@users.noreply.github.com>
2024-02-27 15:35:03 -0600

    Upload todo

---
+++ src/components/TodoList/TodoList.jsx
@@ -0,0 +1,31 @@
+import { TodoFilters } from "../TodoFilters"
+import { Todo } from "../Todo/Todo"
+
+const TodoList = ({todos, handleSetComplete, handleDelete, handleClearComplete, activeFilter, showAllTodos, showActiveTodos, showCompletedTodos  }) =>{
+  return (
+    <div className="flex flex-col mt-7 rounded-lg overflow-hidden shadow-2xl">
+       {todos.map (todo =>{
+           return (
+               <Todo
+                key={todo.id} 
+                todo={todo} 
+                handleSetComplete={handleSetComplete}
+                handleDelete ={handleDelete}
+                />
+           )
+       })}
+       <TodoFilters
+        activeFilter={activeFilter}
+        total ={todos.length}
+        showAllTodos={showAllTodos}
+        showActiveTodos={showActiveTodos}
+        showCompletedTodos={showCompletedTodos}
+        handleClearComplete={handleClearComplete}
+
+       
+       />
+    </div>
+  )
+}
+
+export {TodoList}
